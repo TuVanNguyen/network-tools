df46ff67de90c9632bea1beee77279b99a1a5214
TuVanNguyen <tuvannguyen33@gmail.com>
2020-05-22 16:25:04 -0500

    deleted some files and added exceptions to tcp client

--- tcp/tcp_client.py
+++ tcp/tcp_client.py
@@ -2,17 +2,37 @@
 
 import socket
 import sys
+from customExceptions import PortValueError
 
 def tcp_client(host, port):
-    # create socket object
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # connect to client
-    client.connect((host, port))
-    #send data
-    client.send("GET / HTTP/1.1\r\nHost:{}\r\n\r\n".format(host))
-    #receive some data
-    response = client.recv(4096)
-    print(response)
+    """
+    Creates a tcp connection
+    
+    Args:
+        host (string): target host name (e.g "google.com")
+        port (string): port number, in string form because of input from command-line, must validate that it's number
+    Returns:
+        response (string): response from tcp connection
+    """
+    try:
+        #Check input
+        if not port.isdigit():
+            raise PortValueError("", port)
+
+        # create socket object
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # connect to client
+        client.connect((host, int(port)))
+        #send data
+        client.send("GET / HTTP/1.1\r\nHost:{}\r\n\r\n".format(host))
+        #receive some data
+        response = client.recv(4096)
+        print(response)
+        return response
+    except socket.gaierror:
+        print("Error: {} is an invalid host name".format(host))
+    except PortValueError as e:
+        print(e)
 
 
 
@@ -23,7 +43,7 @@ if __name__ == "__main__":
         sys.argv[2]: target port number (e.g 80)
     """
     target_host = sys.argv[1]
-    target_port = int(sys.argv[2])
+    target_port = sys.argv[2]
     tcp_client(target_host,target_port)
 
 
