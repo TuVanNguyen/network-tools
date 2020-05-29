08aa0a606eb3e2e562fb2f17481fcb253daba1ec
TuVanNguyen <tuvannguyen33@gmail.com>
2020-05-29 12:06:06 -0500

    finished substitute_netcat

--- substitute_netcat/substitute_netcat.py
+++ substitute_netcat/substitute_netcat.py
@@ -13,9 +13,9 @@ class Netcat:
         self.command = False
         self.upload = False
         self.execute = False
-        self.target = ""
+        self.target = None
         self.upload_destination = ""
-        self.port = 0
+        self.port = None
 
     def help(self):
         """
@@ -65,25 +65,26 @@ class Netcat:
             else:
                 assert False, "Unhandled Option"
 
-            # are we going to listen or just send data from stdin?
-            if not self.listen and len(self.target) and self.port > 0:
-                print("Enter a message to send to target")
-                buffer = sys.stdin.readline()
+            if self.target == None or self.port == None:
+                print("Missing valid target or port")
+                sys.exit(0)
+            #client mode 
+            elif not self.listen:
+                buffer = sys.stdin.read()
                 #send data off
                 self.client_sender(buffer)
-
-            #listen and maybe upload, execute, or drop shell back
-
-            if self.listen:
+            #server-mode: listen and maybe upload, execute, or drop shell back
+            elif self.listen:
                 self.server_loop()
 
     def client_sender(self,buffer):
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             #connect to target host
+            print(self.target, self.port)
             client.connect((self.target,self.port))
             if len(buffer):
-                client.sendall(buffer)
+                client.sendall(buffer.encode('utf-8'))
 
             while True:
                 #now wait for data back
@@ -101,13 +102,80 @@ class Netcat:
                 buffer = raw_input("")
                 buffer += "\n"
                 #send it off
-                client.sendall(buffer)
+                client.sendall(buffer.encode('utf-8'))
         except Exception as e:
             print(e)
             client.close()
 
     def server_loop(self):
-        pass
+        print(self.target,self.port)
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((self.target,self.port))
+        listeners = 5
+        server.listen(listeners)
+
+        while True:
+            try:
+                client_socket,addr = server.accept()
+                print("[*] Accepted connection from: {0}:{1}".format(addr[0], addr[1]))
+                client_thread = threading.Thread(target=self.client_handler,
+                        args=(client_socket,))
+                client_thread.start()
+            except KeyboardInterrupt: #graceful shutdown
+                server.close()
+                print("Server is now shutting down")
+                break
+
+    def run_command(self,command):
+        command = command.rstrip() #trim newline
+        # run the command and get the output back
+        try:
+            output = subprocess.check_output(command,stderr=subprocess.STDOUT,
+                    shell=True)
+        except:
+            output = "Failed to execute command.\r\n"
+        # send output back to the client
+        return output
+    
+    def client_handler(self,client_socket):
+        if len(self.upload_destination):
+            file_buffer=""
+            #keep reading data until none is available
+            while True:
+                data = client_socket.recv(1024)
+                if not data:
+                    break
+                else:
+                    file_buffer += data
+                    file_buffer += data
+
+            try:
+                file_descriptor = open(upload_destination, "wb")
+                file_descriptor.write(file_buffer)
+                file_descriptor.close()
+
+            except:
+                client_socket.sendall("Failed to save file to {}\r\n".format(self.upload_destination).encode('utf-8'))
+        
+        #check for command execution
+        if self.execute:
+            output = self.run_command(self.execute)
+            client_socket.sendall(output.encode('utf-8'))
+
+        if self.command:
+            while True:
+                # show simple prompt
+                client_socket.sendall("<Netcat:#>".encode('utf-8'))
+                cmd_buffer = ""
+                while "\n" not in cmd_buffer:
+                    cmd_buffer += client_socket.recv(1024)
+            #send back command output
+            response = self.run_command(cmd_buffer)
+
+            #send back response
+            client_socket.sendall(response.encode('utf-8'))
+            client_socket.close()
+
 
 if __name__ == "__main__":
     nc = Netcat()
